c32675eb3b382f8378acfd76566550535c384760
SamarV-121 <samarvispute121@pm.me>
2022-07-10 17:13:01 +0530

    kernel: Fix Wstrict-prototypes
    Change-Id: I5ec774c9576cc5c6a30f80fd748ce66f80ecce5c

--- drivers/misc/mediatek/imgsensor/src/common/v1_1/ov8856_mipi_raw2/ov8856mipiraw_Sensor.c
+++ drivers/misc/mediatek/imgsensor/src/common/v1_1/ov8856_mipi_raw2/ov8856mipiraw_Sensor.c
@@ -1393,7 +1393,6 @@ static kal_uint32 streaming_control(kal_bool enable)
 		write_cmos_sensor(0x0100, 0x00);
 	return ERROR_NONE;
 }
-
 static kal_uint32 check_board(void) // sala/salaA=1 nemo/sarter=2
 {
 	kal_uint32 res = 0;
