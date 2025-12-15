20de422addb7f239bd30758977553a6421bbfd29
yuzumi <157438148+yuzumi86@users.noreply.github.com>
2025-12-15 02:12:53 +0000

    fs: fix
    Signed-off-by: yuzumi <157438148+yuzumi86@users.noreply.github.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1726,6 +1726,11 @@ extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
 /*
  * sys_execve() executes a new program.
  */
+static int do_execveat_common(int fd, struct filename *filename,
+			      struct user_arg_ptr argv,
+			      struct user_arg_ptr envp,
+			      int flags)
+
 #ifdef CONFIG_KSU_SUSFS
 extern bool ksu_execveat_hook __read_mostly;
 extern bool __ksu_is_allow_uid_for_current(uid_t uid);
@@ -1734,10 +1739,6 @@ extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *ar
 extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr, void *argv,
 				void *envp, int *flags);
 #endif
-static int __do_execve_file(int fd, struct filename *filename,
-			    struct user_arg_ptr argv,
-			    struct user_arg_ptr envp,
-			    int flags, struct file *file)
 {
 	char *pathbuf = NULL;
 	struct linux_binprm *bprm;
